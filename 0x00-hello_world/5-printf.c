67deacacc185537e4cc2add46b96495d79e81287
OGOLA-SOSPETER <captainsos483@gmail.com>
2022-06-02 13:38:23 -0700

    printf commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 /**
-  *this is a short code on the printf
- *Main marks the beginnning
-  * Return: stop
- */
+ *  * main - beg
+ *   *
+ *    * Return: stop
+ *     */
+
 int main(void)
 
-{		printf("with proper grammar, but the outcome is a piece of art,\n");
+{
+
+		printf("with proper grammar, but the outcome is a piece of art,\n");
 
 			return (0);
 
