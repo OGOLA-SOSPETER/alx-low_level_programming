3e67648f0e180bb1cc3b6a0a3057543c0de56a0b
OGOLA-SOSPETER <captainsos483@gmail.com>
2022-06-02 12:50:12 -0700

    commited updates

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,25 +1,23 @@
-#include <stdio.h>
+#include<stdio.h>                                                                                                                       
 
+/**                                                                                                                                     
 
+                                                                                                                                        
 
-/**
+ * main - Function begins                                                                                                               
 
- * main - Function starts
+                                                                                                                                        
 
- *
+ * Return: Function ends                                                                                                                
 
- * Return: Function closes
+                                                                                                                                        
 
- */
+ */                                                                                                                                     
 
+int main(void)                                                                                                                          
 
+{                                                                                                                                       
 
-int main(void)
-
-{
-
-	puts("\"Programming is like building a multilingual puzzle");
-
-	return (0);
-
+write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 59); 
+return (0);
 }
