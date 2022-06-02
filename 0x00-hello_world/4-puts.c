26d57aca5fdd27a8d01694d3b184b3b1fc8a3888
OGOLA-SOSPETER <captainsos483@gmail.com>
2022-06-02 09:09:23 -0700

    c program to output using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,26 @@
+#!/bin/bash
+#include <stdio.h>
+
+
+
+/**
+
+ * main - Function starts
+
+ *
+
+ * Return: Function closes
+
+ */
+
+
+
+int main(void)
+
+{
+
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+
+}
