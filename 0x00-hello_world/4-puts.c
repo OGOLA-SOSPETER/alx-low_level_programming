26b58d5c5a4755085c6cb35297bc74330f01a546
OGOLA-SOSPETER <captainsos483@gmail.com>
2022-06-02 12:56:19 -0700

    updated commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,21 +1,10 @@
-#include <stdio.h>
-
-
-
+#include<stdio.h>
 /**
-
- * main - Function begins
-
- * Return: Function ends
-
- */
-
+*main - Function starts here
+*the return function - function ends here
+*/
 int main(void)
-
 {
-
-	write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 59);
-
-	return(1);
-
+puts(2, "and that piece of art is useful" -Dora Korpar, 2015-10-19\n",59);
+return (1);
 }
