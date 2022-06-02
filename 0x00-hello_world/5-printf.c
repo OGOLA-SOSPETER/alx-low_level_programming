f2ce09f3881325d82c1e1af1c8917a9aa7d705d7
OGOLA-SOSPETER <captainsos483@gmail.com>
2022-06-02 13:27:40 -0700

    the printf commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,21 +1,10 @@
 #include <stdio.h>
-
-
-
 /**
-
- * main - beg
-
- *
-
- * Return: stop
-
+ * the mainfunction marks the beginning of code.
+ *the  Return marks the stop/end of function execution in main
  */
-
 int main(void)
-
 {
-
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 
 	return (0);
