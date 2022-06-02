4fcf036fa47473621150d3062a39494052d71382
OGOLA-SOSPETER <captainsos483@gmail.com>
2022-06-02 12:59:52 -0700

    commit finale

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,25 @@
-#include<stdio.h>
+#include <stdio.h>
+
+
+
 /**
-*main - Function starts here
-*the return function - function ends here
-*/
+
+ * main - Function starts
+
+ *
+
+ * Return: Function closes
+
+ */
+
+
+
 int main(void)
+
 {
-puts(2, "and that piece of art is useful" -Dora Korpar, 2015-10-19\n",59);
-return (1);
+
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+
 }
