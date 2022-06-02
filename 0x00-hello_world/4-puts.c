964e26dd73b15e8795b93a0501b7ab239b3e5da3
OGOLA-SOSPETER <captainsos483@gmail.com>
2022-06-02 13:04:30 -0700

    updated

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,25 +1,10 @@
-#include <stdio.h>
-
-
-
+#include<stdio.h>
 /**
-
- * main - Function starts
-
- *
-
- * Return: Function closes
-
- */
-
-
-
-int main(void)
-
+*main function starts her
+*The return function closes
+*/
+innt main(void)
 {
-
-	puts("\"Programming is like building a multilingual puzzle");
-
-	return (0);
-
+	 puts("\"Programming is like building a multilingual puzzle"); 
+return (0);
 }
