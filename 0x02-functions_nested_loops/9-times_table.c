c794ac3e0b1823fd2f4b5bd2a5883481247a0302
OGOLA-SOSPETER <captainsos483@gmail.com>
2022-06-08 12:16:35 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,5 +1,5 @@
 #include "main.h"
-/**
+/*
  *  * times_table: -prints table
  *    * Return: Always 0.
  */
@@ -38,6 +38,7 @@ _putchar ((c % 10) + '0');
 if (b < 9)
 {
 _putchar(',');
+_putchar(' ');
 }
 }
 }
