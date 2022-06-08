8b1d6b158a582a445739ca70f74cb962cdd462e3
OGOLA-SOSPETER <captainsos483@gmail.com>
2022-06-07 19:52:11 -0700

    commit 10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,8 +1,9 @@
 #include "main.h"
 #include <stdio.h>
 /**
- *  * Add: adds tw integers and prints results
- *   * @i: print int i
+ *   * this is a program to add two numbers
+ *   * add: adds tw integers and prints results
+ *    * @i: print int i
  *    * @k:print int k
  *     * Return: Always 0.
  */
