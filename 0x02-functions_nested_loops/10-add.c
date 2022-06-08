dbca33b8ff1e37f8f6d844db9ffc1be15607becf
OGOLA-SOSPETER <captainsos483@gmail.com>
2022-06-07 19:54:06 -0700

    10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
- *   * this is a program to add two numbers
+ *   this is a program to add two numbers
  *   * add: adds tw integers and prints results
  *    * @i: print int i
  *    * @k:print int k
