acdd7168703c68b98a70d4e2d6760c8634a23b36
OGOLA-SOSPETER <captainsos483@gmail.com>
2022-06-24 14:23:11 -0700

    libmy commit

--- 0x09-static_libraries/2-strncpy.c
+++ 0x09-static_libraries/2-strncpy.c
@@ -1,8 +1,31 @@
 #include "main.h"
 
-int main(void)
+/**
+ * _strncpy - copies a string
+ * @dest: A pointer to a character that will be changed
+ * @src: A pointer to a character that will be changed
+ *@n: value
+ *Return: dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
 
 {
-	_puts("\"my goal was to be the best hacker\"\n\t");
-	return (0);
+	int i;
+
+	i = 0;
+	while (i < n && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+
+	return (dest);
+
 }
