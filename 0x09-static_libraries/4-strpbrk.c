2a8a943e233f47635ad2d63b6faf398c233bb81d
captainsos483@gmail.com <captainsos483@gmail.com>
2022-06-24 06:57:32 -0700

    commit 4-strpbrk.o

---
+++ 0x09-static_libraries/4-strpbrk.c
@@ -0,0 +1,5 @@
+#include "main.h"                                                                                                                       
+int main(void)                                                                                                                         
+{                                                                                             
+       	_puts("\"my goal was to be the best hacker\"\n\t");                                                   return (0);
+}
