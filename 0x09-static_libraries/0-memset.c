425c28f24d4436bd0cb7f8f40f5aead69d1b3fc8
OGOLA-SOSPETER <captainsos483@gmail.com>
2022-06-24 12:59:31 -0700

    0-memset.c

--- 0x09-static_libraries/0-memset.c
+++ 0x09-static_libraries/0-memset.c
@@ -3,6 +3,6 @@
 int main(void)
 
 {
-	_puts("\" my goal was to be the best hacker\"\n\t- OGOLA SOSPETER");
+	_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
 	return (0);
 }
