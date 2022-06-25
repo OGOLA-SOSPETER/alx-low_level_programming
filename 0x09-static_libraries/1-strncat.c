3a92daddc8c2875ee0acda6190b7a1e7d02940a8
OGOLA-SOSPETER <captainsos483@gmail.com>
2022-06-25 00:45:39 -0700

    libmy.a

--- 0x09-static_libraries/1-strncat.c
+++ 0x09-static_libraries/1-strncat.c
@@ -1,8 +1,31 @@
 #include "main.h"
+/**
+ * _strncat - concatenates two strings
+ *@dest: A pointer to a character that will be changed
+ *@src: A pointer to a character that will be changed
+ *@n: value
+ *Return: dest
+ */
 
-int main(void)
+char *_strncat(char *dest, char *src, int n)
 
 {
-	_puts("\"my goal was to be the best hacker\"\n\t");
-	return (0);
+	int i, j;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
+	}
+	dest[i] = '\0';
+
+	return (dest);
 }
