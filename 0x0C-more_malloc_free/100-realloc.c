c799595adfdb3bcf1c1b7350cc3ba8043a10ee7a
OGOLA-SOSPETER <captainsos483@gmail.com>
2022-07-02 01:35:09 -0700

    100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -18,13 +18,13 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		clone = ptr;
 		else
 		{
-		       	return (malloc(new_size)); 
+			return (malloc(new_size)); 
 		}
 		if (new_size == old_size)
 		return (ptr);
 		if (new_size == 0 && ptr != NULL)
 		{
-		       	free(ptr);
+			free(ptr);
 			return (0); 
 		}
 		relloc = malloc(new_size);
