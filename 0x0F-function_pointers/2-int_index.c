93af7f5bf1317b9f0745e5dab760d0c70cb8b19f
OGOLA-SOSPETER <captainsos483@gmail.com>
2022-07-06 12:36:41 -0700

    2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -17,11 +17,11 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	if (array && cmp)
 	{
-	for (i = 0; i < size; i++)
-	{
-	if (cmp(array[i]) != 0)
-	return (i);
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]) != 0)
+			return (i);
+		}
 	}
 	return (-1);
-	}
 }
